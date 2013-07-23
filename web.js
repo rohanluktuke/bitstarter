dccb33165ad340b38c0c38ab03c6375c8db7cbd5
Ubuntu <ubuntu@ip-10-112-30-140.ec2.internal>
2013-07-23 02:52:39 +0000

    using buffer and index

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(255);
+
+buffer = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
